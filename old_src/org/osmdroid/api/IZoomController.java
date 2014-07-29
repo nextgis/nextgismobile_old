c969ec96d96748ffd7b8053b14e9051d42084213
Dmitry Baryshnikov <bishop.dev@gmail.com>
2014-07-30 00:21:38 +0400

    removed unused

--- old_src/org/osmdroid/api/IZoomController.java
+++
@@ -1,8 +0,0 @@
-package org.osmdroid.api;
-
-public interface IZoomController {
-	public void setVisible(boolean bVisible);
-	public void setZoomInEnabled(boolean bEnabled);
-	public void setZoomOutEnabled(boolean bEnabled);
-	public void onZoomChanged();
-}
